e2e25faaad2575ccab824ead0572724f4f7033dd
João Drapala <joaodrapala@digitalsaude.com.br>
2023-10-31 22:00:38 -0300

    initial structure

---
+++ src/main/java/br/com/agrotis/labapi/LabApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.agrotis.labapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/agrotis/labapi/LabApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agrotis.labapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
